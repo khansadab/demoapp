61698810f3e4c134eaa72eeb3cbf518424da2e24
Sadab <sadab4131@gmail.com>
2022-06-14 08:17:28 +0530

    Commited by sadab4131

--- DemoApplication/src/com/ct/demo/DemoApplication.java
+++ DemoApplication/src/com/ct/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.ct.demo;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Allhumdullilha...");
 		System.out.println("Hi Sadab..today date 18 june 2022");
 		System.out.println("Day - Satuerday time 1.10 pm");
