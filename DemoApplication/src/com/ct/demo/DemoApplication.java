c9e5dc61d78d005394c77354d5422738f36b8da7
Sadab <sadab4131@gmail.com>
2022-06-14 08:28:22 +0530

    Commited by Sadab4131

--- DemoApplication/src/com/ct/demo/DemoApplication.java
+++ DemoApplication/src/com/ct/demo/DemoApplication.java
@@ -5,8 +5,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		
 		System.out.println("Allhumdullilha...");
-		System.out.println("Hi Sadab..today date 18 june 2022");
-		System.out.println("Day - Satuerday time 1.10 pm");
+		System.out.println("Hi Sadab..today's date is  18 june 2022");
+		System.out.println("Day - Satuerday time 1.42 pm");
 		System.out.println("Allhumdulillha.... Now I got success... feeling happy to got success after few days.. :) ");
 		
 		int a=2;
