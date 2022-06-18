b04c9b4e2a37395034490b89fc97824150e01faf
Sadab <sadab4131@gmail.com>
2022-06-18 12:17:22 +0530

    Add files via upload

---
+++ DemoApplication/src/com/ct/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.ct.demo;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Allhumdullilha...");
+
+	}
+
+}
