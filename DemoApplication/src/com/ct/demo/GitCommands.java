cda60e5d014021e7f4e043aeb98d1a10e90ccc02
sadabjavadev <sadabjavadev@gmail.com>
2022-06-20 22:02:06 +0530

    commited by sadab4131

---
+++ DemoApplication/src/com/ct/demo/GitCommands.java
@@ -0,0 +1,11 @@
+package com.ct.demo;
+
+public class GitCommands {
+
+	public static void main(String[] args) {
+		System.out.println("Working with Git Commands");
+		System.out.println(); 
+
+	}
+
+}
